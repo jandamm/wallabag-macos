737e1d3789b737969c22fa3d1b9cbe4e387d2420
Jan Dammshäuser <mail@jandamm.de>
2021-01-22 22:04:33 +0100

    Initial Commit

---
+++ Wallabag/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Wallabag
+//
+//  Created by Jan Dammshäuser on 22.01.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
